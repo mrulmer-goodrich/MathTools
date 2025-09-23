87b8a0ca4d27417c037215a9aeccc9d2a1786a7b
mrulmer-goodrich <mr.ulmer-goodrich@cms.k12.nc.us>
2025-09-23 19:20:45 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import BigButton from './components/BigButton.jsx'
 import ScaleFactorModule from './modules/scale/ScaleFactor.jsx'
 import HTableModule from './modules/htable/HTableModule.jsx'
 
-export default function App(){
-  const [route,setRoute] = useState('home')
+export default function App() {
+  const [route, setRoute] = useState('home')
+
+  const Nav = () => (
+    <div className="topnav">
+      <button className="link" onClick={() => setRoute('home')}>Home</button>
+      <span className="sp">•</span>
+      <button className={"link " + (route === 'scale' ? 'active' : '')} onClick={() => setRoute('scale')}>Scale Factor</button>
+      <span className="sp">•</span>
+      <button className={"link " + (route === 'htable' ? 'active' : '')} onClick={() => setRoute('htable')}>H-Table</button>
+    </div>
+  )
+
   return (
     <div>
-      {route==='home' && (
+      <Nav />
+      {route === 'home' && (
         <div className="container">
           <div className="header">
             <div className="brand">UG Math Tools</div>
-            <div className="subtitle">Scale Factor + H-Table trainer (drag-and-drop)</div>
           </div>
           <div className="row">
-            <BigButton onClick={()=>setRoute('scale')}>Scale Factor</BigButton>
-            <BigButton onClick={()=>setRoute('htable')}>H-Table</BigButton>
+            <BigButton onClick={() => setRoute('scale')}>Scale Factor</BigButton>
+            <BigButton onClick={() => setRoute('htable')}>H-Table</BigButton>
           </div>
         </div>
       )}
-      {route==='scale' && <ScaleFactorModule />}
-      {route==='htable' && <HTableModule />}
+      {route === 'scale' && <ScaleFactorModule goHome={() => setRoute('home')} goHTable={() => setRoute('htable')} />}
+      {route === 'htable' && <HTableModule goHome={() => setRoute('home')} goScale={() => setRoute('scale')} />}
     </div>
   )
 }
