725acc8d025da570f4addc09564a4edbd17678a5
mrulmer-goodrich <mr.ulmer-goodrich@cms.k12.nc.us>
2025-09-24 22:07:41 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
-import ErrorBoundary from './ErrorBoundary.jsx'
 
-console.log('Booting Vite app…');
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
 
-createRoot(document.getElementById('root')).render(
-  <ErrorBoundary>
-    <App />
-  </ErrorBoundary>
-)
