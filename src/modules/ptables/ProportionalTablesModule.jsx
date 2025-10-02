f6d903e27fa9ba9b32863587bc104d0b848322ee
mrulmer-goodrich <mr.ulmer-goodrich@cms.k12.nc.us>
2025-10-02 19:33:57 -0400

    Update ProportionalTablesModule.jsx

--- src/modules/ptables/ProportionalTablesModule.jsx
+++ src/modules/ptables/ProportionalTablesModule.jsx
@@ -80,7 +80,7 @@ function __wrapDropSlot(DropSlot){
       if (onClick) onClick(e);
     };
     return <div onClick={handleClick} style={{ display: "inline-block", width: "100%" }}>
-  <DropSlotBase {...rest} test={testFn} onDropContent={onDropContent} onClick={onClick}>{props.children}</DropSlotBase>
+      <DropSlot {...rest} test={testFn} onDropContent={onDropContent} onClick={onClick}>{props.children}</DropSlotBase>
     </div>;
   };
 }
