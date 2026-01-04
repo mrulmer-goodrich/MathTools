c76195066e9e05f93ed8021cc6bf7b846b6ceb4a
mrulmer-goodrich <mr.ulmer-goodrich@cms.k12.nc.us>
2026-01-03 21:18:57 -0500

    Update Algebra.jsx

--- src/algebra/Algebra.jsx
+++ src/algebra/Algebra.jsx
@@ -2,7 +2,7 @@
 // Location: src/algebra/Algebra.jsx
 
 import React, { useState, useEffect } from 'react';
-import DifficultySelection from './components/DifficultySelection';
+import DifficultySelector from './components/DifficultySelector';
 import BaseCamp from './components/BaseCamp';
 import ModulePlayer from './components/ModulePlayer';
 import Header from './components/Header';
